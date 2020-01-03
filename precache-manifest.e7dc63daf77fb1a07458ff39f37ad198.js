3b1d8d95ca5017791906d3bc62d2e4f667fab59a
Maria <cascercerim@gmail.com>
2020-01-03 12:37:14 -0500

    Updates

---
+++ precache-manifest.e7dc63daf77fb1a07458ff39f37ad198.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f81f4cf7a26a90dc6afe26c0ee9a37c4",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "7fa0ff20143b290e2d72",
+    "url": "/clicky-game/static/css/main.63e61236.chunk.css"
+  },
+  {
+    "revision": "73026816a37617a2987b",
+    "url": "/clicky-game/static/js/2.b094a780.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.b094a780.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7fa0ff20143b290e2d72",
+    "url": "/clicky-game/static/js/main.707da065.chunk.js"
+  },
+  {
+    "revision": "b5e3d5480cb174bedaf8",
+    "url": "/clicky-game/static/js/runtime-main.dbb64346.js"
+  },
+  {
+    "revision": "4dd85b08f23ea167bfcae5bacffd7d04",
+    "url": "/clicky-game/static/media/greys.4dd85b08.png"
+  }
+]);
